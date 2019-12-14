4b32d73e6d420dbd2c3ebcfeab9d41ed5a4dd031
gyhdx <gyhdxwang@163.com>
2019-12-14 19:50:02 +0800

    链表中倒数第k个节点

---
+++ src/wf/jianzhi_offer/反转链表.java
@@ -0,0 +1,51 @@
+package wf.jianzhi_offer;
+
+/**
+ * 题目描述
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ */
+public class 反转链表 {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        ListNode list = listNode;
+        list.next = new ListNode(2);
+        list = list.next;
+        list.next = new ListNode(3);
+        list = list.next;
+        list.next = new ListNode(4);
+        list = list.next;
+        list.next = new ListNode(5);
+        ListNode listNode1 = ReverseList(listNode);
+        print(listNode1);
+    }
+
+    /**
+     * 头插法
+     * @param head
+     * @return
+     */
+    public static ListNode ReverseList(ListNode head) {
+        ListNode hesdList=null;
+        if (head==null){
+            return hesdList;
+        }
+
+        while (head!=null){
+            ListNode node = head;
+            head = head.next;
+            node.next=hesdList;
+            hesdList = node;
+        }
+        return hesdList;
+    }
+
+
+    public static void print(ListNode listNode) {
+        if (listNode != null) {
+
+            System.out.println(listNode.val);
+            print(listNode.next);
+        }
+    }
+}
