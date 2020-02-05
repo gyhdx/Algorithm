5ae12f3b832e59fade70a366b50b2cc913bcb49c
gyhdx <gyhdxwang@163.com>
2020-02-05 11:36:42 +0800

    字符串的排列

--- src/wf/jianzhi_offer/字符串的排列.java
+++ src/wf/jianzhi_offer/字符串的排列.java
@@ -9,6 +9,9 @@ import java.util.Collections;
  * 则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
  * 输入描述:
  * 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+ *
+ * 解题思路：
+ *  回溯法
  */
 
 public class 字符串的排列 {
